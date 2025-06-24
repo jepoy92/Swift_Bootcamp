cad1f4ca07259ed13f61cdae422d70dbdd96fe8d
jepoy92 <jeffrey1choi@gmail.com>
2025-06-23 19:03:35 -0700

    Editing dependency injector example

--- Sources/Codespaces/Dependency-Injection.swift
+++ Sources/Codespaces/Dependency-Injection.swift
@@ -6,10 +6,10 @@ class ViewController {
     
     var name: String?
 
-    // Constructor injection - most common, sets the name value with an initializer in the class
-    // init(with name: String) {
-    //     self.name = name
-    // }
+    Constructor injection - most common, sets the name value with an initializer in the class
+    init(with name: String) {
+        self.name = name
+    }
 
     // Setter injection - sets the name value within the class
     // public func setName(with name: String) {
@@ -18,14 +18,14 @@ class ViewController {
 }
 
 // Constructor injection
-// let vc = ViewController(with: "Frank")
+let vc = ViewController(with: "Frank")
 
 // Setter injection
 // let vc = ViewController()
 // vc.setName(with: "Frank")
 
 // Property injection - least common, sets the name value purely by the property outside of the class
-let vc = ViewController()
+// let vc = ViewController()
 vc.name = "Frank"
 
 print(vc.name)
